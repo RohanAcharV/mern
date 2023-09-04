175b5a7ef9af87f04419dce1e727dcfee6def022
RohanAcharV <rohanacharv@gmail.com>
2023-09-04 19:51:22 +0530

    new base url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {useState,useEffect} from 'react';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api' 
+  baseURL: 'https://mern-3dht.onrender.com' 
 });
 // hello
 
